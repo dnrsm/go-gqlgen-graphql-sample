ab20e13249017e0b454819d7db8359c4f7c414a3
dnrsm <daikinarushima@gmail.com>
2021-03-28 03:46:35 +0900

    スキーマにSubscriptionを定義して生成

--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -51,11 +51,19 @@ func (r *queryResolver) Users(ctx context.Context) ([]*model.User, error) {
 	return r.users, nil
 }
 
+func (r *subscriptionResolver) UserAdded(ctx context.Context) (<-chan *model.User, error) {
+	panic(fmt.Errorf("not implemented"))
+}
+
 // Mutation returns generated.MutationResolver implementation.
 func (r *Resolver) Mutation() generated.MutationResolver { return &mutationResolver{r} }
 
 // Query returns generated.QueryResolver implementation.
 func (r *Resolver) Query() generated.QueryResolver { return &queryResolver{r} }
 
+// Subscription returns generated.SubscriptionResolver implementation.
+func (r *Resolver) Subscription() generated.SubscriptionResolver { return &subscriptionResolver{r} }
+
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
+type subscriptionResolver struct{ *Resolver }
