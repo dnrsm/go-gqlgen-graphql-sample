fef18d07d8f8cfabe7e4eae7f714790905aede06
dnrsm <daikinarushima@gmail.com>
2021-03-28 03:23:15 +0900

    resolverを手動で書き換え

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,24 @@
 package graph
 
+import "go-gqlgen-graphql-sample/graph/model"
+
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct {
+	todos      []*model.Todo
+	users      []*model.User
+	lastTodoId int
+}
+
+func NewResolver() *Resolver {
+	users := make([]*model.User, 0)
+	users = append(users, &model.User{ID: "1", Name: "fphilip"})
+	users = append(users, &model.User{ID: "2", Name: "lturanga"})
+
+	return &Resolver{
+		todos: make([]*model.Todo, 0),
+		users: users,
+	}
+}
--- graph/schema.resolvers.go
+++ graph/schema.resolvers.go
@@ -8,14 +8,33 @@ import (
 	"fmt"
 	"go-gqlgen-graphql-sample/graph/generated"
 	"go-gqlgen-graphql-sample/graph/model"
+	"strconv"
 )
 
 func (r *mutationResolver) CreateTodo(ctx context.Context, input model.NewTodo) (*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	var targetUser *model.User
+	for _, user := range r.users {
+		if user.ID == input.UserID {
+			targetUser = user
+			break
+		}
+	}
+	if targetUser == nil {
+		return nil, fmt.Errorf("user with id='%s' not found", input.UserID)
+	}
+	newTodo := &model.Todo{
+		ID:   strconv.Itoa(r.lastTodoId),
+		Text: input.Text,
+		Done: false,
+		User: targetUser,
+	}
+	r.todos = append(r.todos, newTodo)
+	r.lastTodoId++
+	return newTodo, nil
 }
 
 func (r *queryResolver) Todos(ctx context.Context) ([]*model.Todo, error) {
-	panic(fmt.Errorf("not implemented"))
+	return r.todos, nil
 }
 
 // Mutation returns generated.MutationResolver implementation.
